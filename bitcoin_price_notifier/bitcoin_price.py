c1031869aafa40ef411b3093194140bde8d28cab
Amanda Smith <amandasmith@amandas-air.home>
2021-07-25 10:47:33 +0100

    fetching prices

--- bitcoin_price_notifier/bitcoin_price.py
+++ bitcoin_price_notifier/bitcoin_price.py
@@ -1,1 +1,16 @@
-print("hello world")
+import requests
+
+bitcoin_api_url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+params = {"CMC_PRO_API_KEY": "440aeb58-9072-4f7c-8ded-48839f9a8927"}
+headers = {"Accept": "application/json"}
+
+response = requests.get(bitcoin_api_url, params=params, headers=headers)
+bitcoin_full_data = response.json()['data'][0]
+bitcoin_price_data = bitcoin_full_data['quote']['USD']
+
+print(response.status_code)
+print(bitcoin_price_data)
+
+
+
+
