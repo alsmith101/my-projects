df69d6f31eaca965b77c8d7c95b8b1e82f5aca4b
Amanda Smith <amandasmith@amandas-air.home>
2021-07-25 13:15:00 +0100

    create functions and two applets for notifications

---
+++ bitcoin_price_notifier/bitcoin_notifications.py
@@ -0,0 +1,60 @@
+import requests
+import datetime
+import time
+
+bitcoin_api_url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+params = {"CMC_PRO_API_KEY": "440aeb58-9072-4f7c-8ded-48839f9a8927"}
+headers = {"Accept": "application/json"}
+
+
+def get_bitcoin_price():
+    try:
+        response = requests.get(bitcoin_api_url, params=params, headers=headers, timeout=5.0)
+        response.raise_for_status()
+    except (requests.exceptions.HTTPError, requests.exceptions.Timeout) as err:
+        raise Exception(err)
+    bitcoin_full_data = response.json()['data'][0]
+    return float(bitcoin_full_data['quote']['USD']['price'])
+
+
+def post_ifttt_webhook(event, value):
+    ifttt_webhook_url = f"https://maker.ifttt.com/trigger/{event}/with/key/m8jvKqBTcnzokdWG7PuVh7iJlmrqOHcI3LXF6aYCOaG"
+    data = {"value1": value}
+    try:
+        requests.post(ifttt_webhook_url, json=data)
+    except:
+        raise Exception("post to webhook_url failed")
+
+
+def format_bitcoin_price_history(bitcoin_price_history):
+    rows = []
+    for row in bitcoin_price_history:
+        price = row["price"]
+        timestamp = row["timestamp"]
+        rows.append(f"{timestamp}: {price}")
+    return rows
+
+def main():
+
+    while True:
+
+        price = get_bitcoin_price()
+        timestamp = datetime.datetime.today()
+
+        post_ifttt_webhook(event="bitcoin_price_notification", value=price)
+
+        bitcoin_price_history = []
+        bitcoin_price_history.append({"price": price, "timestamp": timestamp})
+
+        if len(bitcoin_price_history)>= 5:
+            post_ifttt_webhook(event="bitcoin_price_history", value=format_bitcoin_price_history(bitcoin_price_history))
+
+        time.sleep(1 * 60)
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
